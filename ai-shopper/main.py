88320ea74d71f197d9ed7e2f1e7418b23f4cdfee
Mudassir <iamimam4u@gmail.com>
2026-05-20 08:37:49 +0530

    fixed cors

--- ai-shopper/main.py
+++ ai-shopper/main.py
@@ -15,7 +15,9 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://fashion-ai-pro.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
